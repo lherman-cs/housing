bfca8b6dae770536de76fa0477140af70c54e26f
Lukas Herman <lherman.cs@gmail.com>
2020-09-16 13:19:41 -0700

    Add selected rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Checkbox,
   Button,
   Paper,
   Table,
@@ -16,14 +17,28 @@ import {InputDialog, InputDialogData} from './components/InputDialog';
 import {NetWorthTrendline} from './components/NetWorthTrendline';
 import {encodeCSV, decodeCSV} from './api/csv'
 
+class Row {
+  selected = true;
+  data: InputDialogData;
+
+  constructor(initialData?: InputDialogData) {
+    if (!initialData) {
+      initialData = new InputDialogData();
+    }
+
+    this.data = initialData;
+  }
+}
+
 function App() {
   const [open, setOpen] = React.useState(false);
-  const [rows, setRows] = React.useState<InputDialogData[]>([]);
+  const [rows, setRows] = React.useState<Row[]>([]);
   const [index, setIndex] = React.useState(-1);
   const [initialData, setInitialData] = React.useState<InputDialogData>(new InputDialogData());
+  const selectedData = rows.filter(e => e.selected).map(e => e.data);
 
   function handleDownloadCSV() {
-    const encodedUri = encodeURI("data:text/csv;charset=utf-8," + encodeCSV(rows));
+    const encodedUri = encodeURI("data:text/csv;charset=utf-8," + encodeCSV(rows.map(e => e.data)));
     window.open(encodedUri);
   }
 
@@ -40,7 +55,7 @@ function App() {
 
     const fr = new FileReader();
     fr.onload = () => {
-      setRows(decodeCSV(fr.result as string, new InputDialogData()));
+      setRows(decodeCSV(fr.result as string, new InputDialogData()).map(e => new Row(e)));
     }
     fr.readAsText(file);
   }
@@ -49,10 +64,10 @@ function App() {
     if (index < 0 || index >= rows.length) {
       setRows([
         ...rows,
-        data.clone()
+        new Row(data.clone())
       ]);
     } else {
-      rows[index] = data.clone();
+      rows[index] = new Row(data.clone());
       setRows([...rows]);
     }
     setOpen(false);
@@ -63,46 +78,78 @@ function App() {
     setOpen(true);
   }
 
-  function handleRemove(i: number) {
+  function handleRemove(e: React.ChangeEvent<HTMLInputElement>, i: number) {
+    e.stopPropagation();
     const left = rows.slice(0, i) || [];
     const right = rows.slice(i + 1) || [];
     setRows([...left, ...right]);
   }
 
-  function handleEdit(i: number) {
+  function handleEdit(e: React.ChangeEvent<HTMLInputElement>, i: number) {
+    e.stopPropagation();
     setIndex(i);
-    setInitialData(rows[i]);
+    setInitialData(rows[i].data);
     setOpen(true);
+  }
+
+  function handleSelectAll(checked: boolean) {
+    setRows(rows.map(e => ({...e, selected: checked})));
+  }
+
+  function handleSelect(i: number) {
+    rows[i].selected = !rows[i].selected;
+    setRows([...rows]);
   }
 
   return (
     <div>
       <div style={{height: '40vh', width: '100%'}}>
-        <NetWorthTrendline data={rows}></NetWorthTrendline>
+        <NetWorthTrendline data={selectedData}></NetWorthTrendline>
       </div>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  checked={selectedData.length > 0 && selectedData.length === rows.length}
+                  onChange={e => handleSelectAll(e.target.checked)}
+                  inputProps={{'aria-label': 'select all desserts'}}
+                />
+              </TableCell>
               <TableCell align="center">Label</TableCell>
               <TableCell align="center">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row, i) => (<TableRow key={i}>
-              <TableCell align="center">{row.label}</TableCell>
-              <TableCell align="center">
-                <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
-                  <IconButton color="primary" aria-label="Edit" component="span" onClick={() => handleEdit(i)}>
-                    <Edit />
-                  </IconButton>
-                  <IconButton color="primary" aria-label="Remove" component="span" onClick={() => handleRemove(i)}>
-                    <Delete />
-                  </IconButton>
-                </ButtonGroup>
+            {rows.map((row, i) => (
+              <TableRow
+                key={i}
+                hover
+                onClick={() => handleSelect(i)}
+                role="checkbox"
+                aria-checked={row.selected}
+                tabIndex={-1}
+                selected={row.selected}
+              >
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={row.selected}
+                  />
+                </TableCell>
+                <TableCell align="center">{row.data.label}</TableCell>
+                <TableCell align="center">
+                  <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+                    <IconButton id="action-edit" color="primary" aria-label="Edit" component="span" onClick={(e: any) => handleEdit(e, i)}>
+                      <Edit />
+                    </IconButton>
+                    <IconButton color="primary" aria-label="Remove" component="span" onClick={(e: any) => handleRemove(e, i)}>
+                      <Delete />
+                    </IconButton>
+                  </ButtonGroup>
 
-              </TableCell>
-            </TableRow>)
+                </TableCell>
+              </TableRow>)
             )}
           </TableBody>
         </Table>
